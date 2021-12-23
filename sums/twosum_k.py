f934c5ec7d46990ef2d50197080b625b5ec29d2f
ante <ante.kotarac@gmail.com>
2021-12-23 22:19:39 +0100

    2 sum less then k

--- sums/twosum_k.py
+++ sums/twosum_k.py
@@ -1,4 +1,4 @@
-def twoSumLessThanK(A, k):
+def twoSumLessThanKCountSort(A, k):
     best = -1
     N = len(A)
     t = [0] * 1001
@@ -16,4 +16,70 @@ def twoSumLessThanK(A, k):
     
     return best
 
-print(twoSumLessThanK([1,2,3,4,5,6,7,8,9,10], 15))
+
+def twoSumLessThanK2Pointer(A, k):
+    best = -1
+    N = len(A)
+    A.sort()
+    lo, hi = 0, N - 1
+    
+    while lo < hi:
+    
+        s = A[lo] + A[hi] 
+        if s > 0 and s < k and s > best:
+            best = s
+        if s >= k:
+            hi -= 1
+        else:
+            lo += 1
+    
+    return best
+
+from bisect import bisect_left
+
+def twoSumLessThanKBisect(A, k):
+    best = -1
+    N = len(A)
+    A.sort()
+    
+    for i in range(N):
+        j = bisect_left(A, k- A[i], i) - 1
+        if j > i:
+            s = A[i] + A[j] 
+            if s > 0 and s < k and s > best:
+                best = s
+    
+    return best
+
+class BinarySearchSolution:
+    def search(self, A, lo, v, k):
+        hi = len(A) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            s = v + A[mid]
+            if s == k:
+                return mid
+            if s < k:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return lo
+            
+    def twoSumLessThanK(self, A, k):
+        best = -1
+        N = len(A)
+        A.sort()
+        
+        for i in range(N):
+            j = self.search(A, i, A[i], k) - 1
+            if j > i:
+                s = A[i] + A[j] 
+                if s > 0 and s < k and s > best:
+                    best = s
+        
+        return best
+
+print(twoSumLessThanKCountSort([1,2,3,4,5,6,7,8,9,10], 15))
+print(twoSumLessThanK2Pointer([1,2,3,4,5,6,7,8,9,10], 15))
+print(twoSumLessThanKBisect([1,2,3,4,5,6,7,8,9,10], 15))
+print(BinarySearchSolution().twoSumLessThanK([1,2,3,4,5,6,7,8,9,10], 15))
