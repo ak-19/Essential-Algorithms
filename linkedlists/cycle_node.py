34a5db5ed9c669d31c11d20dc3208937dd1811e1
ante <ante.kotarac@gmail.com>
2022-01-19 15:48:53 +0100

    find cycle in linked list

---
+++ linkedlists/cycle_node.py
@@ -0,0 +1,19 @@
+def detectCycle(head):
+    '''If exists, find cycle node in linked list'''
+    def get_node():
+        fast = slow = head        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next            
+            if fast == slow: return slow
+        return None
+    
+    node = get_node()
+        
+    if node is None: return None
+    
+    while node != head:            
+        node = node.next
+        head = head.next
+        
+    return node
