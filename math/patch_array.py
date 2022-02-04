4c8e5ab61f3b935c52d7ce4abd38fb5440ee5e2f
ante <ante.kotarac@gmail.com>
2022-02-04 21:50:40 +0100

    patch array

---
+++ math/patch_array.py
@@ -0,0 +1,18 @@
+def patch_array(A, n):    
+    '''
+    Given array A, find number of missing numbers that are ned to 
+    construct continious sequence of numbers [1,n] 
+    '''            
+    patches = 0
+    i = 0
+    miss = 1
+    
+    while miss <= n:        
+        if i < len(A) and A[i] <= miss:
+            miss += A[i]
+            i += 1            
+        else:
+            miss += miss
+            patches += 1
+    
+    return patches
