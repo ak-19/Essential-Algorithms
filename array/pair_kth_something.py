05dbf5be4e30b5d03899c00395cbd4158a5df4f9
ante <ante.kotarac@gmail.com>
2022-02-21 14:31:08 +0100

    k smallest sum pairs

--- array/pair_kth_something.py
+++ array/pair_kth_something.py
@@ -18,3 +18,23 @@ def kth_smallest_absolute_distance_pair(A, k):
         else: lo = mid + 1
         
     return lo
+
+from heapq import heappop, heappush
+
+def k_smallest_sums_pairs(A, B, k):
+    seen = set()
+    result = []
+    N1 = len(A)
+    N2 = len(B)
+    pq = [(A[0]+ B[0], 0, 0)]
+    
+    while len(result) < k and pq:
+        v, i1, i2 = heappop(pq)
+        result.append((A[i1], B[i2]))
+        
+        for i, j in [(i1 + 1, i2), (i1, i2 + 1)]:
+            if i < N1 and j < N2 and (i, j) not in seen:
+                heappush(pq, (A[i]+B[j], i, j))
+                seen.add((i, j))
+    
+    return result    
