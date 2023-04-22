5dcfdffee4b0f52cf8c8484a030b7784020a9311
ante <ante.kotarac@gmail.com>
2023-04-22 21:46:32 +0200

    alternative lps with reverse string

--- strings/palindromes/max_palindrom_subseq.py
+++ strings/palindromes/max_palindrom_subseq.py
@@ -16,3 +16,14 @@ def max_palindromic_subsequence_length(s) -> int:
                 
             max_palindrome = max(max_palindrome, dp[i][i + size])             
     return max_palindrome
+# 2. alternative solution wich uses reverse string
+# Given a string and its reverse, find the length of its Longest Palindromic Subsequence (LPS).
+def max_palindromic_subsequence_length(s, s2):
+    N = len(s)
+    dp = [[0]*(N + 1) for _ in range(N + 1)]
+    for i in range(N + 1):
+        for j in range(N + 1):
+            if 0 not in [i,j]: 
+                if s[i - 1] == s2[j - 1]: dp[i][j] = 1 + dp[i - 1][j - 1]
+                else: dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+    return dp[N][N]
